a8772d75e7b1234e57fc54210e420fb918440318
Swastik Kumar <swastikkumar166@gmail.com>
2024-06-03 17:12:21 +0530

    Updated Saving

--- training.py
+++ training.py
@@ -2,12 +2,14 @@
 
 import pickle
 import models
+import os
 
 def train_model(model_name, best_params, X_train, y_train):
-    clf = models.get_classifier(model_name, best_params, X_train, y_train)
+    clf = models.get_reg(model_name, best_params, X_train, y_train)
     clf.fit(X_train, y_train)
     return clf
 
-def save_model(model, filename):
+def save_model(model, path):
+    filename = os.path.join(path, 'model.pkl')
     with open(filename, 'wb') as f:
         pickle.dump(model, f)
